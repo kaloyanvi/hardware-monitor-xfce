4325d0f50212b94748ff720ab499acf20e9f0832
Kaloyan Videlov <kvidelov@protonmail.com>
2020-12-15 15:24:45 +0100

    Fixed bug with CPU temperature.

--- widgets/cpu.sh
+++ widgets/cpu.sh
@@ -10,21 +10,24 @@ readonly ICON="${DIR}/icons/cpu.png"
 declare -r CPU_ARRAY=($(awk '/MHz/{print $4}' /proc/cpuinfo | cut -f1 -d"."))
 # Number of logical CPU
 readonly NUM_OF_CPUS="${#CPU_ARRAY[@]}"
+# Tempretature
+readonly CPU_TEMP="$(sensors | grep -A 0 SMBUSMASTER | cut -f2- -d: | sed "s/[^0-9'.]//g")"
 
 # Tooltip
 MORE_INFO="<tool>"
-MORE_INFO+="┌ $(grep "model name" /proc/cpuinfo | cut -f2 -d ":" | sed -n 1p | sed -e 's/^[ \t]*//')\n" # CPU vendor, model, clock
+MORE_INFO+="$(grep "model name" /proc/cpuinfo | cut -f2 -d ":" | sed -n 1p | sed -e 's/^[ \t]*//')\n" # CPU vendor, model, clock
+MORE_INFO+="\n┌─\t\t\t  Live Data\n"
+
 STEP=0 # to generate CPU numbers (eg. CPU0, CPU1 ...)
 for CPU in "${CPU_ARRAY[@]}"; do
   STDOUT=$(( STDOUT + CPU ))
-  MORE_INFO+="├─ CPU ${STEP}: ${CPU} MHz\n"
+  MORE_INFO+="├─ CPU ${STEP}:\t\t\t\t  ${CPU} MHz\n"
   let STEP+=1
 done
-MORE_INFO+="└─ Temperature: $(sensors | grep -A 0 SMBUSMASTER | cut -c26-30)"
+MORE_INFO+="└─ Temperature:\t\t  ${CPU_TEMP}°C"
 MORE_INFO+="</tool>"
 STDOUT=$(( STDOUT / NUM_OF_CPUS )) # calculate average clock speed
 STDOUT=$(awk '{$1 = $1 / 1024; printf "%.2f%s", $1, "GHz"}' <<< "${STDOUT}")
-TEMP="$(sensors | grep -A 0 SMBUSMASTER | cut -c26-30)"
 
 # Panel
 if [[ $(file -b "${ICON}") =~ PNG|SVG ]]; then
@@ -36,8 +39,8 @@ if [[ $(file -b "${ICON}") =~ PNG|SVG ]]; then
 else
   INFO="<txt>"
 fi
-INFO+=" ${STDOUT}"
-INFO+=" - $TEMP"C"  "
+INFO+="${STDOUT}"
+INFO+=" - $CPU_TEMP"°C"  "
 INFO+="</txt>"
 
 # Output panel
